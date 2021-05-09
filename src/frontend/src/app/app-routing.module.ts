f5f00506b20a8f70026c2660243c5c633ff1cd5a
Me <vendramin.mitja@gmail.com>
2021-05-09 13:08:10 +0200

    zaslonska maska za dodajanje psa dodana #45

--- src/frontend/src/app/app-routing.module.ts
+++ src/frontend/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from './components/login/login.component';
+import { DodajPsaComponent } from './components/dodaj-psa/dodaj-psa.component';
 
 const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
+  },
+  {
+    path: 'dodaj_psa',
+    component: DodajPsaComponent,
   }
 ];
 
--- src/frontend/src/app/app.module.ts
+++ src/frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -7,6 +8,7 @@ import { ToBeDeletedComponent } from './components/to-be-deleted/to-be-deleted.c
 import { ToBeDeletedPipe } from './pipes/to-be-deleted/to-be-deleted.pipe';
 import { LoginComponent } from './components/login/login.component';
 import { NavbarLoginSignupComponent } from './components/navbars/navbar-login-signup/navbar-login-signup.component';
+import { DodajPsaComponent } from './components/dodaj-psa/dodaj-psa.component';
 
 @NgModule({
   declarations: [
@@ -14,10 +16,12 @@ import { NavbarLoginSignupComponent } from './components/navbars/navbar-login-si
     ToBeDeletedComponent,
     ToBeDeletedPipe,
     LoginComponent,
-    NavbarLoginSignupComponent
+    NavbarLoginSignupComponent,
+    DodajPsaComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     NgbModule
   ],
