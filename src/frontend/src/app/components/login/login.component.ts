7934b7ca42bdaef07c53f36ca552ee857f7046a1
jakmar17 <jakob.marusic17@gmail.com>
2021-05-09 22:58:43 +0200

    dodana prijava z JWT žetonom #76 #74

--- src/frontend/src/app/components/login/login.component.ts
+++ src/frontend/src/app/components/login/login.component.ts
@@ -1,27 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/services/login/login.service';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
   submitted = false;
   emptyPassword = true;
   username = '';
   showSuccess = false;
   isOnline = false; // to bila funkcija
   public prijava = {
-    email: '',
-    geslo: ''
+    username: '',
+    geslo: '',
   };
   public napakaNaObrazcu = '';
 
-  constructor() { }
+  constructor(private loginService: LoginService, private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public login() {
+    this.loginService
+      .login(this.prijava.username, this.prijava.geslo)
+      .then((data) => {
+        // todo popravi kam linkat
+        if (data == true) {
+          alert("Prijava uspešna: " + this.loginService.userLoggedIn.username);
+          this.router.navigate(['/']);
+        } else {
+          this.napakaNaObrazcu = 'Napaka pri prijavi';
+        }
+      })
+      .catch(() => (this.napakaNaObrazcu = 'Napaka pri prijavi'));
   }
-
-
 }
