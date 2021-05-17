5ec5914c3d11ea10cd868068e353672632d58237
jakmar17 <jakob.marusic17@gmail.com>
2021-05-17 11:59:00 +0200

    popravljen UI #

--- src/frontend/src/app/components/zacetna-stran/zacetna-stran.component.ts
+++ src/frontend/src/app/components/zacetna-stran/zacetna-stran.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-zacetna-stran',
   templateUrl: './zacetna-stran.component.html',
-  styleUrls: ['./zacetna-stran.component.scss']
+  styleUrls: ['./zacetna-stran.component.scss'],
 })
 export class ZacetnaStranComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  public navigateToSignUp(tip: string): void {
+    this.router.navigate(['/signup', tip]);
   }
-
 }
