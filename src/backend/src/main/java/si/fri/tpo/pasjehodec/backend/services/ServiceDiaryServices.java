2ec2980a4a28fe301a787799f47b921995e9e03e
Primoz19 <pg1499@student.uni-lj.si>
2021-05-23 23:35:10 +0200

    Dodan service diary API klici #44

--- src/backend/src/main/java/si/fri/tpo/pasjehodec/backend/services/ServiceDiaryServices.java
+++ src/backend/src/main/java/si/fri/tpo/pasjehodec/backend/services/ServiceDiaryServices.java
@@ -2,7 +2,10 @@ package si.fri.tpo.pasjehodec.backend.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import si.fri.tpo.pasjehodec.backend.database.entities.DogoEntity;
 import si.fri.tpo.pasjehodec.backend.database.entities.ServiceDiaryEntity;
+import si.fri.tpo.pasjehodec.backend.database.entities.ServiceEntity;
+import si.fri.tpo.pasjehodec.backend.database.entities.users.UserEntity;
 import si.fri.tpo.pasjehodec.backend.database.repositories.ServiceDiaryRepository;
 
 @Service
@@ -11,8 +14,17 @@ public class ServiceDiaryServices {
 
     private final ServiceDiaryRepository serviceDiaryRepository;
 
-    public ServiceDiaryEntity createNewServiceDiary(ServiceDiaryEntity entity) {
+    public ServiceDiaryEntity createNewServiceDiary(ServiceDiaryEntity entity, DogoEntity dogoEntity, ServiceEntity serviceEntity) {
+        entity.setDogo(dogoEntity);
+        entity.setService(serviceEntity);
+        entity.setStatus("Narocena storitev");
         return serviceDiaryRepository.save(entity);
+    }
+
+    public ServiceDiaryEntity addRating(Integer id, int rating) {
+        ServiceDiaryEntity serviceDiaryEntity = serviceDiaryRepository.getOne(id);
+        serviceDiaryEntity.setAssess(rating);
+        return serviceDiaryEntity;
     }
 
 }
