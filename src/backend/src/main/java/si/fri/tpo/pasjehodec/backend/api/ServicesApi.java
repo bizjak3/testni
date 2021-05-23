4242ffff52ed305c4081dba1892640a03eb45059
Primoz19 <pg1499@student.uni-lj.si>
2021-05-24 00:00:41 +0200

    fix se enkrat #44

--- src/backend/src/main/java/si/fri/tpo/pasjehodec/backend/api/ServicesApi.java
+++ src/backend/src/main/java/si/fri/tpo/pasjehodec/backend/api/ServicesApi.java
@@ -113,10 +113,10 @@ public class ServicesApi {
 
 
     @PostMapping("post-service-diary")
-    public ResponseEntity<ServiceDiaryDto> postServiceDiary(@RequestBody DogoEntity dogoEntity,
+    public ResponseEntity<ServiceDiaryDto> postServiceDiary(@RequestBody ServiceDiaryEntity serviceDiaryEntity,
+                                                            @RequestBody DogoEntity dogoEntity,
                                                             @RequestBody ServiceEntity serviceEntity) {
 
-        ServiceDiaryEntity serviceDiaryEntity = new ServiceDiaryEntity();
         ServiceDiaryEntity entity = serviceDiaryServices.createNewServiceDiary(serviceDiaryEntity, dogoEntity, serviceEntity);
 
         return ResponseEntity.ok(
