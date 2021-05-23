b845571f1cc773b0f73215cac8b7637c246d3153
Primoz19 <pg1499@student.uni-lj.si>
2021-05-23 23:36:47 +0200

    Izboljsan message Api #54

--- src/backend/src/main/java/si/fri/tpo/pasjehodec/backend/services/MessageService.java
+++ src/backend/src/main/java/si/fri/tpo/pasjehodec/backend/services/MessageService.java
@@ -3,20 +3,29 @@ package si.fri.tpo.pasjehodec.backend.services;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import si.fri.tpo.pasjehodec.backend.database.entities.MessageEntity;
+import si.fri.tpo.pasjehodec.backend.database.entities.users.UserEntity;
 import si.fri.tpo.pasjehodec.backend.database.repositories.MessageRepository;
+import si.fri.tpo.pasjehodec.backend.database.repositories.UserRepository;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
 public class MessageService {
 
     private final MessageRepository messageRepository;
+    private final UserRepository userRepository;
 
     public MessageEntity[] getAllMessages() {
 
         return messageRepository.findAll().toArray(new MessageEntity[0]);
     }
 
-    public MessageEntity createNewMessage(MessageEntity entity) {
+    public MessageEntity createNewMessage(MessageEntity entity, UserEntity sender, Integer recipientId) {
+
+        entity.setSender(sender);
+        entity.setRecipient(userRepository.getOne(recipientId));
+        entity.setCreated(LocalDateTime.now());
         return messageRepository.save(entity);
     }
 }
