5ffe0316155518417acea20a2bd79028f075b63b
Primoz19 <pg1499@student.uni-lj.si>
2021-05-23 23:05:03 +0200

    Dodan API klic za vse uporabnikove pse #46

--- src/backend/src/main/java/si/fri/tpo/pasjehodec/backend/api/DogoApi.java
+++ src/backend/src/main/java/si/fri/tpo/pasjehodec/backend/api/DogoApi.java
@@ -6,15 +6,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.*;
+import si.fri.tpo.pasjehodec.backend.database.entities.DogoEntity;
 import si.fri.tpo.pasjehodec.backend.database.entities.users.UserEntity;
 import si.fri.tpo.pasjehodec.backend.database.entities.users.UserType;
 import si.fri.tpo.pasjehodec.backend.dtos.mappers.DogoEntityMapper;
 import si.fri.tpo.pasjehodec.backend.dtos.models.dogo.DogoDto;
 import si.fri.tpo.pasjehodec.backend.services.DogoService;
+
+import java.util.Arrays;
 
 @RestController
 @RequestMapping("/dogos/")
@@ -31,7 +32,21 @@ public class DogoApi {
     })
     public ResponseEntity<DogoDto> postDogo(@RequestBody DogoDto dogo, @Parameter(hidden = true) @AuthenticationPrincipal UserEntity user) {
         var entity = dogoEntityMapper.castDogoEntityFromDto(dogo);
-        entity = dogoService.addDogo(entity, entity.getOwner());
+        entity = dogoService.addDogo(entity, user);
         return ResponseEntity.ok(dogoEntityMapper.castDogoDtoFromEntity(entity));
+    }
+
+    @GetMapping("get-users-dogos")
+    public ResponseEntity<DogoDto[]> getUsersDogos(@AuthenticationPrincipal UserEntity user) {
+        return ResponseEntity.ok(
+                dogoService.getUsersDogos(user).stream()
+                .map(dogoEntityMapper::castDogoDtoFromEntity)
+                .toArray(DogoDto[]::new)
+        );
+    }
+
+    @GetMapping("location")
+    public ResponseEntity<DogoDto> getDogoLocation(@Parameter DogoEntity dogoEntity) {
+        return ResponseEntity.ok(dogoEntityMapper.castDogoDtoFromEntity(dogoEntity));
     }
 }
